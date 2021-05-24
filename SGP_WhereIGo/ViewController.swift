a5120ab733a658c13df2c4fc000e8a8bb7f453a4
Game <63137718+gunwoong1996@users.noreply.github.com>
2021-05-24 18:29:17 +0900

    UI UPDATE

---
+++ SGP_WhereIGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SGP_WhereIGo
+//
+//  Created by KPU_GAME on 2021/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SGP_WhereIGoTests/SGP_WhereIGoTests.swift
@@ -0,0 +1,33 @@
+//
+//  SGP_WhereIGoTests.swift
+//  SGP_WhereIGoTests
+//
+//  Created by KPU_GAME on 2021/05/11.
+//
+
+import XCTest
+@testable import SGP_WhereIGo
+
+class SGP_WhereIGoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
